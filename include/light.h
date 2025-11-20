6732d624e2c097b51d79c90afd3d7473b591dd2e
ducpham2310797 <duc.pham2310797@hcmut.edu.vn>
2025-11-20 17:29:25 +0700

    Update from local zip

---
+++ include/light.h
@@ -0,0 +1,20 @@
+#ifndef LIGHT_H
+#define LIGHT_H
+
+#include <Arduino.h>
+#include "FreeRTOS.h"
+#include "queue.h"
+#include "global.h"
+// Chân kết nối LDR
+#define LIGHT_PIN 16  // dùng chân ADC1_CH1 trên Yolo UNO
+
+
+// Queue chứa trạng thái ánh sáng (true = sáng, false = tối)
+extern QueueHandle_t xLightQueue;
+
+// Hàm task cảm biến ánh sáng
+void Light_Task(void *pvParameters);
+
+// Khởi tạo cảm biến
+
+#endif
